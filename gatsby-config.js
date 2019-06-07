e840cb86e2d9277a6b126be5c1ca70b40af9cd04
Patryk Kalinowski <patrislav7@gmail.com>
2019-06-07 23:09:57 +0300

    Branding (icon + theme colour)

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,10 +19,10 @@ module.exports = {
         name: `kalinowski.dev`,
         short_name: `kalinowski.dev`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#37474f`,
+        theme_color: `#37474f`,
         display: `minimal-ui`,
-        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `./src/icon.png`, // This path is relative to the root of the site.
       },
     },
     {
